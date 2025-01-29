aa9451ff3e570fd0dc44b77c8b8e3f25408dafa3
gaganpreet001 <gaganpreethunjan28@gmail.com>
2025-01-29 12:05:49 +0530

    first api (GET)

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationTest.Model;
+using System.Collections.Generic;
+
+namespace WebApplicationTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        List<Category> listofCategories = new List<Category>
+        {
+            new Category{Id=1 ,Title="Samsung",DisplayOrder=1 },
+            new Category{Id=2 ,Title="Xiomi", DisplayOrder=2},
+            new Category{Id=3 ,Title="Nokia", DisplayOrder=3},
+            new Category{Id=4 ,Title="Apple", DisplayOrder=4},
+            new Category{Id=5,Title="Vivo", DisplayOrder=5},
+
+
+
+
+        };
+        [HttpGet]
+        public IEnumerable<Category>Get()
+            { return listofCategories; }
+    }
+}
---
+++ Model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplicationTest.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
